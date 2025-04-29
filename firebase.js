cb1ac26978d7f46e6105466b179d017104a98bbc
ken <kensukemoneymoney@gmail.com>
2025-04-28 21:33:38 -0600

    Fix a line to read env file

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@
 const admin = require("firebase-admin");
 
 // Replace with your service account key path
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
